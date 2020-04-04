a3c7e2ea1f5f0152c663a87b5b31187b388a2da8
bofomiso <maycor16@yahoo.com>
2020-04-03 21:23:25 -0700

    basic layout setup

---
+++ dogapp/lib/screens/direction.dart
@@ -0,0 +1,10 @@
+import 'package:dogapp/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Direction extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //screen that is going to direct the user if they are logged in or not
+    return Home();
+  }
+}
