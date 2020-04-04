c36195de4f3bad68d717acc52e42af7f3f5491b7
bofomiso <maycor16@yahoo.com>
2020-04-04 00:27:11 -0700

    home screen not pretty

--- dogapp/lib/screens/home/home.dart
+++ dogapp/lib/screens/home/home.dart
@@ -3,8 +3,35 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Home Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dog App'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Image.asset('assets/dog.png'),
+      ),
+      bottomNavigationBar:ButtonBar(
+          alignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            FloatingActionButton(
+              child: Text('Play'),
+              onPressed: null,
+            ),
+            FloatingActionButton(
+              child: Text('Profile'),
+              onPressed: null,
+            ),
+            FloatingActionButton(
+              child: Text('Log'),
+              onPressed: null
+            ),
+            FloatingActionButton(
+              child: Text('Park'),
+              onPressed:null
+            ),  
+          ],
+        ), 
     );
   }
 }
